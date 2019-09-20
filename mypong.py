100301321bd4014cfc175f6ab1f5e50266d78dd1
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-09-19 23:33:23 -0400

    Funçoes de criar objeto prontas

--- mypong.py
+++ mypong.py
@@ -4,57 +4,79 @@
 # fonte Press Start 2P https://www.fontspace.com/codeman38/press-start-2p
 # som pontuação https://freesound.org/people/Kodack/sounds/258020/
 
-import turtle
 import os
+from tkinter import _default_root
+import turtle
 
-# desenhar tela
-screen = turtle.Screen()
-screen.title("My Pong")
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.tracer(0)
+playing = True
 
-#desenhar raquete 1
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto(-350,0)
+# Função para criar a janela (screen)
+def create_screen(title, width, height):
+    screen = turtle.Screen()
+    screen.title(title)
+    screen.bgcolor("black")
+    screen.setup(width=width, height=height)
+    screen.tracer(0)
+    return screen
 
-#desenhar raquete 2
-paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
-paddle_2.shape("square")
-paddle_2.color("white")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto(350,0)
+# Função que é chamada ao fechar a janela
+# Usada para finalizar o game loop
+def close_screen():
+    global playing
+    playing = not playing
 
-#desenhar bola
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0,0)
-ball.dx = 2
-ball.dy = 2
+# Função para criar o turtle das raquetes
+def create_paddle(x, y, width, height, color):
+    paddle = turtle.Turtle()
+    paddle.speed(0)
+    paddle.shape("square")
+    paddle.color(color)
+    paddle.shapesize(stretch_wid=width, stretch_len=height)
+    paddle.penup()
+    paddle.goto(x, y)
+    return paddle
 
-# pontuação
-score_1 = 0
-score_2 = 0
+# Função para criar o turtle da bola
+def create_ball(x, y, color):
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("circle")
+    ball.color(color)
+    ball.penup()
+    ball.goto(x, y)
+    ball.dx = 0.5
+    ball.dy = 0.5
+    return ball
 
-# head-up display da pontuação
-hud = turtle.Turtle()
-hud.speed(0)
-hud.shape("square")
-hud.color("white")
-hud.penup()
-hud.hideturtle()
-hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Press Start 2P",24,"normal") )
+def create_hud():
+    hud = turtle.Turtle()
+    hud.speed(0)
+    hud.shape("square")
+    hud.color("white")
+    hud.penup()
+    hud.hideturtle()
+    hud.goto(0, 250)
+    return hud
+
+screen = create_screen("My Pong", 800, 600)
+root = screen.getcanvas().winfo_toplevel()
+root.protocol("WM_DELETE_WINDOW", close_screen)
+
+paddle_1 = create_paddle(-350, 0, 5, 1, "red")
+paddle_2 = create_paddle(350, 0, 5, 1, "blue")
+
+ball = create_ball(0, 0, "white")
+
+north_wall = create_paddle(0, 300, 0.5, 40, "red")
+south_wall = create_paddle(0, -295, 0.5, 40, "red")
+left_wall = create_paddle(-400, 0, 30, 0.5, "red")
+right_wall = create_paddle(395, 0, 30, 0.5, "red")
+
+left_player_score = 0
+right_player_score = 0
+
+hud = create_hud()
+hud.write("{} : {}".format(left_player_score, right_player_score), align="center", font=("Press Start 2P",24,"normal") )
 
 # mover raquete 1
 def paddle_1_up():
@@ -96,40 +118,38 @@ screen.onkeypress(paddle_1_down,"s")
 screen.onkeypress(paddle_2_up,"Up")
 screen.onkeypress(paddle_2_down,"Down")
 
-while True:
-    screen.update()
-
+while playing:
     # movimentação da bola
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
     #colisão com parede superior
     if ball.ycor() > 290:
-        os.system("afplay bounce.wav&")
+        # os.system("afplay bounce.wav&")
         ball.sety(290)
         ball.dy *= -1
     
     #colisão com parede inferior
     if ball.ycor() < -280:
-        os.system("afplay bounce.wav&")
+        # os.system("afplay bounce.wav&")
         ball.sety(-280)
         ball.dy *= -1
 
     #colisão com parede esquerda
     if ball.xcor() < -390:
-        score_2 += 1
+        right_player_score += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
-        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
+        hud.write("{} : {}".format(left_player_score, right_player_score), align="center", font=("Press Start 2P",24,"normal") )
+        # os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
         ball.goto(0,0)
         ball.dx *= -1
     
     #colisão com parede direita
     if ball.xcor() > 390:
-        score_1 += 1
+        left_player_score += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
-        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
+        hud.write("{} : {}".format(left_player_score, right_player_score), align="center", font=("Press Start 2P",24,"normal") )
+        # os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
         ball.goto(0,0)
         ball.dx *= -1
 
@@ -137,9 +157,11 @@ while True:
     # colisão com raquete 1
     if ball.xcor() < -330 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
         ball.dx *= -1     
-        os.system("afplay bounce.wav&")   
+        # os.system("afplay bounce.wav&")   
     
     # colisão com raquete 2
     if ball.xcor() > 330 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        # os.system("afplay bounce.wav&")
+
+    screen.update()
